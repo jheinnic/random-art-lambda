d551caf71eb3c559152412b7137f91b8ff1a028e
John Heinnickel <john.heinnickel@thermofisher.com>
2022-09-30 19:21:57 -0700

    Apply PBufPlotModel to PBufRegionMap refactoring that missed this file

--- function/src/plotting/protobuf/PBufRegionMapDecoder.ts
+++ function/src/plotting/protobuf/PBufRegionMapDecoder.ts
@@ -2,17 +2,16 @@ import { Injectable } from "@nestjs/common"
 
 import { IRegionMap, IRegionMapDecoder } from "../../painting/interface"
 import { PBufRegionMap } from "./PBufRegionMap.js"
-import { PointPlotData, PointPlotDocument } from "./plot_mapping_pb.mjs"
+import { PointPlotDocument } from "./plot_mapping_pb.mjs"
 
 @Injectable()
 export class PBufRegionMapDecoder implements IRegionMapDecoder {
   async provide (modelBuf: Uint8Array): Promise<IRegionMap> {
-    const plotDocument: PointPlotDocument =
-      PointPlotDocument.deserializeBinary(modelBuf)
-    const plotData: PointPlotData | undefined = plotDocument.getData()
+    const plotDocument = PointPlotDocument.deserializeBinary(modelBuf)
+    const plotData = plotDocument.getData()
     if (plotData === undefined) {
       throw new Error("Plot document block lacks a payload?")
     }
-    return new PBufPlotModel(plotData)
+    return new PBufRegionMap(plotData)
   }
 }
