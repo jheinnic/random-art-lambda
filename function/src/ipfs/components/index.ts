bda43415fccaefed7651579188b1263d84a36655
John Heinnickel <john.heinnickel@thermofisher.com>
2022-10-16 11:23:18 -0700

    IPFS DI Maintenance

--- function/src/ipfs/components/index.ts
+++ function/src/ipfs/components/index.ts
@@ -1,3 +1,1 @@
-export { FsBlockstoreConfiguration } from "./FsBlockstoreConfiguration.js"
-export { ModuleInstanceConfiguration } from "./ModuleInstanceConfiguration.js"
-export { FsBlockstorage, buildLruCache } from "./FsBlockstore.js"
+export { FsBlockstore, buildLruCache } from "./FsBlockstore.js"
