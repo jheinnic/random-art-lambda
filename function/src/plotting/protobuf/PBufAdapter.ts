332cb17489c248dd5b9ae478a0189009d64de554
John Heinnickel <john.heinnickel@thermofisher.com>
2022-10-16 23:06:40 -0700

    EArly round of debugging after last refactor

--- function/src/plotting/protobuf/PBufAdapter.ts
+++ function/src/plotting/protobuf/PBufAdapter.ts
@@ -1,11 +1,12 @@
 import { Inject, Injectable } from "@nestjs/common"
+import * as fs from "fs"
 import { CID } from "multiformats/cid"
 
 import { IpldRegionMapRepository } from "../components/IpldRegionMapRepository.js"
 import { PlottingModuleTypes } from "../di/typez.js"
-import { IRegionMapBuilder } from "../interface/IRegionMapBuilder"
+import { IRegionMapBuilder } from "../interface/IRegionMapBuilder.js"
 import { Numeric, RegionBoundaries } from "../interface/RegionMapSchemaTypes.js"
-import { PointPlotData, RefPoint } from "./plot_mapping_pb"
+import { PointPlotData, PointPlotDocument, RefPoint } from "./plot_mapping_pb.mjs"
 
 @Injectable()
 export class PBufAdapter {
@@ -14,7 +15,18 @@ export class PBufAdapter {
   ) {
   }
 
-  async transfer (source: PointPlotData, chunkHeight: number): Promise<CID> {
+  public async import (sourceFile: string, chunkHeight: number): Promise<CID> {
+    const modelBuf = fs.readFileSync(sourceFile)
+    const plotDocument = PointPlotDocument.deserializeBinary(modelBuf)
+    const plotData = plotDocument.getData()
+    if ((plotData === undefined) || (plotData === null)) {
+      console.error("Not Plot Data!")
+      throw new Error()
+    }
+    return await this.transfer(plotData, chunkHeight)
+  }
+
+  public async transfer (source: PointPlotData, chunkHeight: number): Promise<CID> {
     return await this.repository.import(
       (builder: IRegionMapBuilder) => {
         const resolution = source.getResolution()
