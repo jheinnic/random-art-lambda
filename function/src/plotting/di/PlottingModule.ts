c3ab734f3b1651e4ce80691d86a13d0f65ff0b7e
John Heinnickel <john.heinnickel@thermofisher.com>
2023-05-01 12:08:08 -0700

    WIP: Plotting packaging

--- function/src/plotting/di/PlottingModule.ts
+++ function/src/plotting/di/PlottingModule.ts
@@ -10,33 +10,33 @@ import { PlottingModuleConfiguration } from "./PlottingModuleConfiguration.js"
 import { ConfigurableModuleClass } from "./PlottingModuleDefinition.js"
 import { PlottingModuleTypes } from "./PlottingModuleTypes.js"
 
-@Module({
+@Module( {
   providers: [
-  {
-  provide: PlottingModuleTypes.IRegionMapRepository,
-  useClass: IpldRegionMapRepository
-  },
-  {
-  provide: PlottingModuleTypes.IRegionMapSchemaDsl,
-  useClass: IpldRegionMapSchemaDsl
-  },
-  {
-  provide: PlottingModuleTypes.ProtoBufAdapterFactory,
-  useClass: PBufAdapterFactory
-  },
-  {
-  provide: PlottingModuleTypes.InjectedBlockStore,
-  useFactory: (config: PlottingModuleConfiguration): Blockstore => {
-  return config.blockStore
-  },
-  inject: [PlottingModuleTypes.PlottingModuleConfiguration]
-  }
+    {
+      provide: PlottingModuleTypes.IRegionMapRepository,
+      useClass: IpldRegionMapRepository
+    },
+    {
+      provide: PlottingModuleTypes.IRegionMapSchemaSerdes,
+      useClass: IpldRegionMapSchemaDsl
+    },
+    {
+      provide: PlottingModuleTypes.ProtoBufAdapterFactory,
+      useClass: PBufAdapterFactory
+    },
+    {
+      provide: PlottingModuleTypes.InjectedBlockStore,
+      useFactory: ( config: PlottingModuleConfiguration ): Blockstore => {
+        return config.blockStore
+      },
+      inject: [ PlottingModuleTypes.PlottingModuleConfiguration ]
+    }
   ],
-  exports: [PlottingModuleTypes.IRegionMapRepository, PlottingModuleTypes.ProtoBufAdapterFactory]
-  })
+  exports: [ PlottingModuleTypes.IRegionMapRepository, PlottingModuleTypes.ProtoBufAdapterFactory ]
+} )
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class, @typescript-eslint/no-unused-vars
 export class PlottingModule extends ConfigurableModuleClass {
-  registerAsync (options: PlottingModuleAsyncOptions): DynamicModule {
-    return super.registerAsync(options)
+  registerAsync( options: PlottingModuleAsyncOptions ): DynamicModule {
+    return super.registerAsync( options )
   }
 }
