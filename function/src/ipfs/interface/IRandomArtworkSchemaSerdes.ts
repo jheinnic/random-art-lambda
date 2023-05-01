b547841389a67411a3d04269a6444b390791f6a3
John Heinnickel <john.heinnickel@thermofisher.com>
2023-05-01 12:03:38 -0700

    Removig relocated ipfs-based model interfaces

--- function/src/ipfs/interface/IRandomArtworkSchemaSerdes.ts
+++
@@ -1,5 +0,0 @@
-import { ISchemaSerdes } from './ISchemaSerdes.js'
-import { ModelEnvelope } from './ModelEnvelope.js'
-// import { RandomArtworkSpec } from './RandomArtworkSpec.js'
-
-export type IRandomArtworkSchemaSerdes = ISchemaSerdes<{ repoVersion: string, model: [] }, ModelEnvelope>
