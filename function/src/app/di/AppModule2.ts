6ca2015b386cd6c6c388af85efeda4845bd4ab13
John Heinnickel <john.heinnickel@thermofisher.com>
2024-06-14 12:36:02 +0000

    Reduce wiring to get something executable proven

---
+++ function/src/app/di/AppModule2.ts
@@ -0,0 +1,29 @@
+import { Module } from "@nestjs/common"
+import { Blockstore } from "interface-blockstore"
+
+import { IpfsModule, IpfsModuleTypes } from "../../ipfs/di/index.js"
+import { PlottingModule, PlottingModuleConfiguration, PlottingModuleConfigurationFactory } from "../../plotting/di/index.js"
+import { PaintingModule } from "../../painting/di/index.js"
+import { AppService } from "../components/AppService.js"
+import { AppServiceTwo } from "../components/AppServiceTwo.js"
+import { SharedBlockstoresModule } from "./SharedBlockstoresModule.js"
+import { SharedBlockstoresModuleTypes } from "./SharedBlockstoresModuleTypes.js"
+
+@Module( {
+  imports: [
+    SharedBlockstoresModule,
+    PlottingModule.registerAsync( {
+      imports: [ SharedBlockstoresModule ],
+      useFactory: ( blockstore: Blockstore ): PlottingModuleConfiguration =>
+        new PlottingModuleConfiguration( blockstore ),
+      inject: [ SharedBlockstoresModuleTypes.SharedMapBlockstore ]
+    } ),
+    PaintingModule
+  ],
+  providers: [ AppService ],
+  exports: [ AppService, PlottingModule, PaintingModule ]
+  // providers: [ AppService, AppServiceTwo ],
+  // exports: [ AppServiceTwo, AppService, PlottingModule, PaintingModule ]
+} )
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class AppModule { }
