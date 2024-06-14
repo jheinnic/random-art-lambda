4b858e16238e9d49514d18f9eff30f2f3a2f3b74
John Heinnickel <john.heinnickel@thermofisher.com>
2024-06-14 12:45:50 +0000

    Function overloading in an interface

--- function/src/plotting/interface/IRegionMapBuilder.ts
+++ function/src/plotting/interface/IRegionMapBuilder.ts
@@ -4,7 +4,10 @@ export interface IRegionMapBuilder {
   pixelRef: ( pixelRef: "Center" | "TopLeft" ) => IRegionMapBuilder
   imageSize: ( width: number, height: number ) => IRegionMapBuilder
   // chunkHeight: ( height: number ) => IRegionMapBuilder
-  regionBoundary: ( boundary: RegionBoundaries | RegionBoundaryFractions ) => IRegionMapBuilder
+  regionBoundary: {
+    ( boundary: RegionBoundaries ): IRegionMapBuilder;
+    ( boundary: RegionBoundaryFractions ): IRegionMapBuilder
+  }
   xByRows: ( rowOrderX: number[] ) => IRegionMapBuilder
   yByRows: ( rowOrderY: number[] ) => IRegionMapBuilder
 }
