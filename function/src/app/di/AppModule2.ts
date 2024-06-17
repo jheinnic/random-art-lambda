30b921f775ebd42f05c37f1f432b2914708ed14a
John Heinnickel <john.heinnickel@thermofisher.com>
2024-06-17 20:27:05 +0000

    Enable Express web requests and inject a rudimentary controller

--- function/src/app/di/AppModule2.ts
+++ function/src/app/di/AppModule2.ts
@@ -6,6 +6,7 @@ import { PlottingModule, PlottingModuleConfiguration, PlottingModuleConfiguratio
 import { PaintingModule } from "../../painting/di/index.js"
 import { AppService } from "../components/AppService.js"
 import { AppServiceTwo } from "../components/AppServiceTwo.js"
+import { AppController } from "../components/AppController.js"
 import { SharedBlockstoresModule } from "./SharedBlockstoresModule.js"
 import { SharedBlockstoresModuleTypes } from "./SharedBlockstoresModuleTypes.js"
 
@@ -20,10 +21,10 @@ import { SharedBlockstoresModuleTypes } from "./SharedBlockstoresModuleTypes.js"
     } ),
     PaintingModule
   ],
-  providers: [ AppService ],
-  exports: [ AppService, PlottingModule, PaintingModule ]
-  // providers: [ AppService, AppServiceTwo ],
-  // exports: [ AppServiceTwo, AppService, PlottingModule, PaintingModule ]
+  providers: [ AppService, AppController ],
+  exports: [ AppService, AppController, PlottingModule, PaintingModule ]
+  // providers: [ AppService, AppServiceTwo, AppController ],
+  // exports: [ AppServiceTwo, AppService, AppController, PlottingModule, PaintingModule ]
 } )
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class AppModule { }
