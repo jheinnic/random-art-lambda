44104c3cf4cc9d78a690a6260b34ab914c80ff32
John Heinnickel <john.heinnickel@thermofisher.com>
2025-01-13 22:47:13 +0000

    Formatting

--- function/src/main.ts
+++ function/src/main.ts
@@ -1,13 +1,12 @@
-const app = await NestFactory.create<NestExpressApplication>( AppModule )
 import { NestFactory } from "@nestjs/core"
 import { NestExpressApplication } from "@nestjs/platform-express"
 
 import { AppService } from "./app/components/AppService.js"
 import { AppModule } from "./app/di/AppModule2.js"
 
-async function bootstrap (): Promise<void> {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestExpressApplication>( AppModule )
-  const appService = app.get(AppService)
-  console.log(appService.testCommand())
+  const appService = app.get( AppService )
+  console.log( appService.testCommand() )
 }
 bootstrap()
