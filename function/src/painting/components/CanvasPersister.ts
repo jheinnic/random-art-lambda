25f71d45bee76f439a9d64f06419eb034509271e
John Heinnickel <john.heinnickel@thermofisher.com>
2025-02-04 14:18:49 +0000

    Async closing that works for CanvasPersister

--- function/src/painting/components/CanvasPersister.ts
+++ function/src/painting/components/CanvasPersister.ts
@@ -8,22 +8,53 @@ export class CanvasPersister { // implements ICompleteObserver {
     private readonly canvas: Canvas,
     private readonly streamOut: fs.WriteStream,
   ) {
-    this.streamOut.on("end", () => {
-      console.log(`Wrote out png with ${this.streamOut.bytesWritten} bytes`)
-    })
-    this.streamOut.on("error", (err: any) => {
-      console.error(`Error received after writing ${this.streamOut.bytesWritten} bytes`)
-      console.error(err)
-    })
+    // this.streamOut.on( "end", () => {
+    //   console.log( `Wrote out png with ${ this.streamOut.bytesWritten } bytes` )
+    //   this.streamOut.close()
+    // } )
+    // this.streamOut.on( "finish", () => {
+    //   console.log( `Wrote out png with ${ this.streamOut.bytesWritten } bytes` )
+    //   this.streamOut.close()
+    // } )
+    // this.streamOut.on( "error", ( err: any ) => {
+    //   console.error( `Error received after writing ${ this.streamOut.bytesWritten } bytes` )
+    //   console.error( err )
+    // } )
   }
 
-  public finish (): void {
-    // const streamIn = this.canvas.createPNGStream()
-    // const streamOut = this.streamOut
-    // streamIn.on("error", (err: any) => {
-    // console.error(err)
-    // streamOut.close()
-    // })
-    this.canvas.createPNGStream().pipe(this.streamOut)
+  private closeStream() {
+    this.streamOut.close()
+  }
+
+  public async finish(): Promise<void> {
+    return new Promise<void>( ( resolve, reject ) => {
+      try {
+        const pngStream = this.canvas.createPNGStream()
+
+        pngStream.on( "error", ( err ) => {
+          console.error( "PNG stream error:", err )
+          this.closeStream()
+          reject( err ) // Reject the promise on error
+        } )
+
+        this.streamOut.on( "error", ( err ) => {
+          console.error( "Write stream error:", err )
+          this.closeStream()
+          reject( err ) // Reject the promise on error
+        } )
+
+        this.streamOut.on( "finish", () => { // Use 'finish' for reliability
+          console.log( "Write stream finished" )
+          this.closeStream()
+          resolve() // Resolve the promise when done
+        } )
+
+        pngStream.pipe( this.streamOut )
+      } catch ( err ) {
+        console.error( "Error in finish:", err )
+        this.closeStream()
+        reject( err ) // Reject the promise on error
+      }
+    } )
   }
 }
