8c8e92e9bd26ad67e7e6411d3772db9484c3aa9d
John Heinnickel <john.heinnickel@thermofisher.com>
2025-02-04 14:19:20 +0000

    Still canned main routines

--- function/src/app/components/Main.ts
+++ function/src/app/components/Main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from "@nestjs/core"
+import { sha256 as hash } from "multiformats/hashes/sha2"
 
+import { CID } from "multiformats"
 import { AppModule } from "../di/index.js"
 import { AppServiceTwo } from "./AppServiceTwo.js"
 
-async function bootstrap (): Promise<void> {
-  const app = await NestFactory.createApplicationContext(AppModule)
-  const appSvc = app.get(AppServiceTwo)
-  console.log(appSvc)
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.createApplicationContext( AppModule )
+  const appSvc = app.get( AppServiceTwo )
+  console.log( appSvc )
+
+  appSvc.testRun()
+
+  // const regionMap = appSvc.testRepo( CID.parse( "zdpuAsQEbAYrfbrgcR7EgDarTSGePziWyX3m8jL4gmJtn9V1v" ) )
+  // const regionMap = appSvc.testRepo( CID.parse( "zdpuB381N99CGFwmnVCXfubQANt8ZnSW3sk3Hy8YEYyB1fRGP" ) )
+  // const regionMap = appSvc.testRepo( CID.parse( "zdj7WVuzaY8f6J53yJpjM8H1hFq9QRZKBWKukdGZFMiScp8AM" ) )
+  // const regionMap = appSvc.testRepo( CID.parse( "QmeXewWTbGUnvAPQ5VUcJ2uF3PX1uWbZg1Yjk9EJzQqzXF" ) )
+  // console.log( regionMap )
 }
 
-bootstrap().catch((x) => console.error(x))
+bootstrap().catch( ( x ) => console.error( x ) )
+// tn / 9V / 1v / zdpuAsQEbAYrfbrgcR7EgDarTSGePziWyX3m8jL4gmJ
