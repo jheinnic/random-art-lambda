5cd1d3c2484872d5d2ab2805a2af54fcc146018d
John Heinnickel <john.heinnickel@thermofisher.com>
2025-02-04 14:04:41 +0000

    Synchronizing plot_map semantics and adding a few canned sizes

--- python/plot_mapping/q4.py
+++ python/plot_mapping/q4.py
@@ -1,12 +1,12 @@
 from precise_grid import compute_dimensions, plot_points
 from plot_mapping_pb2 import *
 
-width = 1024
-height = 1024
-xmin = -1.0
-xmax = 1.0
-ymin = -1.0
-ymax = 1.0
+width = 640
+height = 640
+xmin = 0.40
+xmax = 0.60
+ymin = -0.60
+ymax = -0.40
 
 pixel_lengths, frame_lengths, pixel_heights, frame_heights = compute_dimensions(width, xmin, xmax, height, ymin, ymax)
 pixel_points, frame_points = plot_points(pixel_lengths, frame_lengths, pixel_heights, frame_heights)
@@ -24,7 +24,7 @@ fDocument.data.uniform = False
 fDocument.data.rows.extend(frame_points[0].flatten())
 fDocument.data.columns.extend(frame_points[1].flatten())
 
-file = open("qdoc.proto", "wb")
+file = open("qdoc-040_060_640-n060_n040_640.proto", "wb")
 file.write(
   fDocument.SerializeToString())
 file.close()
