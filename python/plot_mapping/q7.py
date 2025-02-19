1727c3b6aba430a4a5acf1f8ab9083eb8893c03c
John Heinnickel <john.heinnickel@thermofisher.com>
2025-02-19 00:17:08 +0000

    More sample inputs

---
+++ python/plot_mapping/q7.py
@@ -0,0 +1,31 @@
+from precise_grid import compute_dimensions, plot_points
+from plot_mapping_pb2 import *
+
+width = 1024
+height = 1024
+xmin = -1.0
+xmax = 1.0
+ymin = -1.0
+ymax = 1.0
+
+pixel_lengths, frame_lengths, pixel_heights, frame_heights = compute_dimensions(width, xmin, xmax, height, ymin, ymax)
+pixel_points, frame_points = plot_points(pixel_lengths, frame_lengths, pixel_heights, frame_heights)
+
+
+fDocument = PointPlotDocument()
+fDocument.data.pixelRef = RefPoint.CENTER
+fDocument.data.resolution.pixelWidth = width;
+fDocument.data.resolution.pixelHeight = height;
+fDocument.data.mapped_region.top = ymax;
+fDocument.data.mapped_region.bottom = ymin;
+fDocument.data.mapped_region.left = xmin;
+fDocument.data.mapped_region.right = xmax;
+fDocument.data.uniform = False
+fDocument.data.rows.extend(frame_points[0].flatten())
+fDocument.data.columns.extend(frame_points[1].flatten())
+
+file = open("qdoc-10_10_1024-10_10_1024.proto", "wb")
+file.write(
+  fDocument.SerializeToString())
+file.close()
+
