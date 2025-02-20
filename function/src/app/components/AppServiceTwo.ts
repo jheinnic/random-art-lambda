595e8775c3ecc8a04bcb9c0664070325b8df3551
John Heinnickel <john.heinnickel@thermofisher.com>
2025-02-20 16:30:04 +0000

    More convenience refactoring

--- function/src/app/components/AppServiceTwo.ts
+++ function/src/app/components/AppServiceTwo.ts
@@ -2,7 +2,8 @@ import { Inject, Injectable } from "@nestjs/common"
 import { CID } from "multiformats"
 import { Canvas } from "canvas"
 import { sha256 as hasher } from "multiformats/hashes/sha2"
-import * as fs from "fs"
+import crypto from 'crypto'
+import fs from 'fs'
 
 import { PaintingModuleTypes } from "../../painting/di/index.js"
 import { PlottingModuleTypes } from "../../plotting/di/index.js"
@@ -12,8 +13,11 @@ import { PBufAdapterFactory } from "../../plotting/protobuf/PBufAdapterFactory.j
 import { CanvasPixelPainter } from "../../painting/components/CanvasPixelPainter.js"
 import { CanvasPersister } from "../../painting/components/CanvasPersister.js"
 import { GenModelArtist } from "../../painting/components/GenModelArtist.js"
-import { GenModel, newPicture } from "../../painting/components/genjs6.js"
+import { GenModel, newPicture, oldPicture } from "../../painting/components/genjs6.js"
 import { PBufAdapter } from "../../plotting/protobuf/PBufAdapter.js"
+
+type Task = { genModel: GenModel, fileName: string }
+type Region = { name: string, regionMap: IRegionMap }
 
 @Injectable()
 export class AppServiceTwo {
@@ -48,7 +52,7 @@ export class AppServiceTwo {
     return this.cidCache.get( cid )
   }
 
-  public async testRun1(): Promise<void> {
+  public async testRun0(): Promise<void> {
     const beginString: string = "Happy Thanksgiving Burger"
     const beginBuf: Buffer = Buffer.from( beginString )
     const beginArray: Uint8Array = Uint8Array.from( beginBuf )
@@ -64,48 +68,83 @@ export class AppServiceTwo {
       const prefStr = Buffer.from( prefix ).toString( 'hex' )
       const suffStr = Buffer.from( suffix ).toString( 'hex' )
       const fileName = `./${ prefStr }_${ suffStr }.png`
-      await this.doOne( prefix, suffix, regionMap, fileName )
+      const genModel: GenModel = newPicture( [ ...prefix ], [ ...suffix ] )
+      await this.doOne( genModel, regionMap, fileName )
     }
   }
 
-  public async testRun(): Promise<void> {
+  public testRun1(): Promise<void> {
     const workList: { prefix: string, suffix: string }[] =
       JSON.parse( fs.readFileSync( "source.list" ).toString() )
-    const adapters: PBufAdapter[] = [
-      this.adapterFactory.adapt( "./qdoc4.proto" ),
-      this.adapterFactory.adapt( "./qdoc5.proto" ),
-      this.adapterFactory.adapt( "./qdoc6.proto" )
-    ]
+    const taskList: Task[] = workList.map(
+      ( task ) => {
+        let buf: Buffer = Buffer.from( task.prefix, "hex" )
+        const prefix: Uint8Array = Uint8Array.from( buf )
+        buf = Buffer.from( task.suffix, "hex" )
+        const suffix: Uint8Array = Uint8Array.from( buf )
+        return {
+          genModel: newPicture( [ ...prefix ], [ ...suffix ] ),
+          fileName: `${ task.prefix }_${ task.suffix }.png`
+        }
+      }
+    )
 
-    const regionMaps: IRegionMap[] = [
-      adapters[ 0 ].asRegionMap(), adapters[ 1 ].asRegionMap(), adapters[ 2 ].asRegionMap()
-    ]
-    const regions: { name: string, regionMap: IRegionMap }[] = [
-      { regionMap: regionMaps[ 0 ], name: "qdoc4" },
-      { regionMap: regionMaps[ 1 ], name: "qdoc5" },
-      { regionMap: regionMaps[ 2 ], name: "qdoc6" },
-    ]
+    const sourceNames: string[] = [ "qdoc4", "qdoc5", "qdoc6" ]
+    const regionList: Region[] = sourceNames.map(
+      ( sourceName: string ) => {
+        const adapter: PBufAdapter = this.adapterFactory.adapt( `./${ sourceName }.proto` )
+        return { name: sourceName, regionMap: adapter.asRegionMap() }
+      }
+    )
 
-    let task: { prefix: string, suffix: string }
-    let region: { name: string, regionMap: IRegionMap }
+    return this.runCombinations( taskList, regionList )
+  }
 
-    for ( task of workList ) {
-      let buf: Buffer = Buffer.from( task.prefix, "hex" )
-      const prefix: Uint8Array = Uint8Array.from( buf )
-      buf = Buffer.from( task.suffix, "hex" )
-      const suffix: Uint8Array = Uint8Array.from( buf )
+  public testRun(): Promise<void> {
+    const workList: { phrase: string }[] =
+      JSON.parse( fs.readFileSync( "source2.list" ).toString() )
+    const taskList: Task[] = workList.map(
+      ( task ) => {
+        const fileName: string = crypto.createHash( 'md5' )
+          .update( task.phrase )
+          .digest()
+          .toString( 'base64' )
+          .replaceAll( '/', '_' )
+          .replaceAll( '=', '' )
+        return {
+          genModel: oldPicture( task.phrase ),
+          fileName: `${ fileName }.png`
+        }
+      }
+    )
 
+    // const sourceNames: string[] = [ "qdoc4", "qdoc5", "qdoc6" ]
+    const sourceNames: string[] = [ "rdoc" ]
+    const regionList: Region[] = sourceNames.map(
+      ( sourceName: string ) => {
+        const adapter: PBufAdapter = this.adapterFactory.adapt( `./${ sourceName }.proto` )
+        return { name: sourceName, regionMap: adapter.asRegionMap() }
+      }
+    )
+
+    return this.runCombinations( taskList, regionList )
+  }
+
+  public async runCombinations( taskList: Task[], regionList: Region[] ): Promise<void> {
+    let task: Task
+    for ( task of taskList ) {
       await Promise.all(
-        regions.map( ( region ) => {
-          const fileName = `./${ region.name }/${ task.prefix }_${ task.suffix }.png`
-          return this.doOne( prefix, suffix, region.regionMap, fileName )
-        } )
+        regionList.map(
+          ( region: Region ) => {
+            const fileName = `./${ region.name }/${ task.fileName }`
+            return this.doOne( task.genModel, region.regionMap, fileName )
+          }
+        )
       )
     }
   }
 
-  private async doOne( prefix: Uint8Array, suffix: Uint8Array, regionMap: IRegionMap, fileName: string ) {
-    const genModel: GenModel = newPicture( [ ...prefix ], [ ...suffix ] )
+  private async doOne( genModel: GenModel, regionMap: IRegionMap, fileName: string ): Promise<void> {
     const canvas: Canvas = new Canvas( regionMap.pixelWidth, regionMap.pixelHeight, 'image' )
     const canvasPainter: CanvasPixelPainter = new CanvasPixelPainter( canvas )
     const artist: GenModelArtist = new GenModelArtist( genModel, canvasPainter )
@@ -113,6 +152,5 @@ export class AppServiceTwo {
     const stream = fs.createWriteStream( fileName )
     const persister: CanvasPersister = new CanvasPersister( canvas, stream )
     await persister.finish()
-    console.log( fileName )
   }
 }
