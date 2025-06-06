05e6949f8ca4ae2a6328bb548439604c70193e3e
John Heinnickel <john.heinnickel@thermofisher.com>
2025-06-06 04:43:06 +0000

    More exporation with fabbricated test inputs.

--- function/src/app/components/Main.ts
+++ function/src/app/components/Main.ts
@@ -12,11 +12,21 @@ async function bootstrap(): Promise<void> {
 
   appSvc.testRun()
 
+  // This has not been tried
   // const regionMap = appSvc.testRepo( CID.parse( "zdpuAsQEbAYrfbrgcR7EgDarTSGePziWyX3m8jL4gmJtn9V1v" ) )
+
+  // This exists, but does not follow the model
   // const regionMap = appSvc.testRepo( CID.parse( "zdpuB381N99CGFwmnVCXfubQANt8ZnSW3sk3Hy8YEYyB1fRGP" ) )
+
+  // These do not exist
   // const regionMap = appSvc.testRepo( CID.parse( "zdj7WVuzaY8f6J53yJpjM8H1hFq9QRZKBWKukdGZFMiScp8AM" ) )
   // const regionMap = appSvc.testRepo( CID.parse( "QmeXewWTbGUnvAPQ5VUcJ2uF3PX1uWbZg1Yjk9EJzQqzXF" ) )
+
+  // This is current
+  // const regionMap = appSvc.testRepo( CID.parse( "bafyreicqvrftolzvv3jhmqptkezch7f7dlfomu6mmv3cihqmerg274x3ky" ) )
   // console.log( regionMap )
+
+  //appSvc.testRepoSave()
 }
 
 bootstrap().catch( ( x ) => console.error( x ) )
