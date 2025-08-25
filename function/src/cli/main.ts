467177f7e7f12647793cb48f8317b15b9d5cfb75
John Heinnickel <john.heinnickel@thermofisher.com>
2025-08-25 08:12:00 +0000

    Increasing error handling visibility

--- function/src/cli/main.ts
+++ function/src/cli/main.ts
@@ -3,7 +3,15 @@ import { CliAppModule } from "./app/Module.js"
 
 async function bootstrap(): Promise<void> {
    // or, if you only want to print Nest's warnings and errors
-   await CommandFactory.run(CliAppModule, ["warn", "error"])
+   const retval = await CommandFactory.run(CliAppModule, [
+      "warn",
+      "error",
+      "log",
+      "debug",
+      "verbose",
+      "fatal",
+   ])
+   console.log(retval)
 }
 
 try {
