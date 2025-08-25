095241a277f2a6d6f4226c1906da68ea8182eb72
John Heinnickel <john.heinnickel@thermofisher.com>
2025-08-25 22:26:39 +0000

    Initial Plotting!

--- function/src/painting/message/RandomArtTaskCall.ts
+++ function/src/painting/message/RandomArtTaskCall.ts
@@ -6,18 +6,42 @@ import { RandomArtTaskReply } from "./RandomArtTaskReply.js"
 import type { Prefix } from "./Prefix.js"
 import type { Suffix } from "./Suffix.js"
 
-export class RandomArtTaskCall {
+export abstract class AbstractRandomArtTaskCall {
+   public abstract readonly inputKind: string
+
    public readonly correlationId: string
 
-   constructor(
-      public readonly prefix: Prefix,
-      public readonly suffix: Suffix,
-      public readonly regionMap: CID,
-   ) {
+   constructor(public readonly regionMap: CID) {
       this.correlationId = uuidv4()
    }
 
    public prepareReply(canvas: Canvas): RandomArtTaskReply {
       return new RandomArtTaskReply(this.correlationId, canvas)
+   }
+
+   public prepareError(message: string): RandomArtTaskReply {
+      return new RandomArtTaskReply(this.correlationId, undefined, message)
+   }
+}
+
+export class RandomArtTaskCall extends AbstractRandomArtTaskCall {
+   public readonly inputKind: "PrefixSuffix" = "PrefixSuffix"
+   constructor(
+      public readonly prefix: Prefix,
+      public readonly suffix: Suffix,
+      readonly regionMap: CID,
+   ) {
+      super(regionMap)
+   }
+}
+
+export class RandomArtTaskWordsCall extends AbstractRandomArtTaskCall {
+   public readonly inputKind: "WordPair" = "WordPair"
+   constructor(
+      public readonly prefix: string,
+      public readonly suffix: string,
+      readonly regionMap: CID,
+   ) {
+      super(regionMap)
    }
 }
