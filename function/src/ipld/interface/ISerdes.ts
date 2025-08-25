764b74065079ea84776530651ebe64186eb2c734
John Heinnickel <john.heinnickel@thermofisher.com>
2025-08-25 07:43:08 +0000

    Autoformatting

--- function/src/ipld/interface/ISerdes.ts
+++ function/src/ipld/interface/ISerdes.ts
@@ -2,27 +2,27 @@ import { ByteView, BlockView } from "multiformats"
 import { RepresentDomainPair } from "./RepresentDomainPair.js"
 
 export interface ISerdes<RD extends RepresentDomainPair> {
-  /**
-   * Transform-to-representation and Encode
-   */
-  encodeModel: ( model: RD[1] ) => Promise<BlockView<RD[0]>>
+   /**
+    * Transform-to-representation and Encode
+    */
+   encodeModel: (model: RD[1]) => Promise<BlockView<RD[0]>>
 
-  /**
-   * Decode
-   * @param bytes 
-   * @returns Decoded Block
-   */
-  bytesToBlock: ( bytes: ByteView<RD[0]> ) => Promise<BlockView<RD[0]>>
-  /**
-   * Decode and Transform-to-domain
-   * @param bytes 
-   * @returns Domain model from a decoded block
-   */
-  bytesToDomain: ( bytes: ByteView<RD[0]> ) => Promise<RD[1]>
-  /**
-   * Transform-to-domain
-   * @param block 
-   * @returns Domain model
-   */
-  blockToDomain: ( block: BlockView<RD[0]> ) => Promise<RD[1]>
+   /**
+    * Decode
+    * @param bytes
+    * @returns Decoded Block
+    */
+   bytesToBlock: (bytes: ByteView<RD[0]>) => Promise<BlockView<RD[0]>>
+   /**
+    * Decode and Transform-to-domain
+    * @param bytes
+    * @returns Domain model from a decoded block
+    */
+   bytesToDomain: (bytes: ByteView<RD[0]>) => Promise<RD[1]>
+   /**
+    * Transform-to-domain
+    * @param block
+    * @returns Domain model
+    */
+   blockToDomain: (block: BlockView<RD[0]>) => Promise<RD[1]>
 }
--- function/src/ipld/interface/ISerdesFactory.ts
+++ function/src/ipld/interface/ISerdesFactory.ts
@@ -1,8 +1,9 @@
-import { StringKeys } from 'simplytyped'
+import { StringKeys } from "simplytyped"
 import { ISerdes } from "./ISerdes.js"
-import { RepresentDomainPair } from "./RepresentDomainPair.js"
 import { ISchemaSignature } from "./ISchemaSignature.js"
 
 export interface ISerdesFactory<RDS extends ISchemaSignature> {
-    getProduction<P extends StringKeys<RDS>>( rootProduction: P ): ISerdes<RDS[P]>
+   getProduction: <P extends StringKeys<RDS>>(
+      rootProduction: P,
+   ) => ISerdes<RDS[P]>
 }
