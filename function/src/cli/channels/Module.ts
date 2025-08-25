6002a22a9bc0524e99c0a4b6d0503d1a92db2ce6
John Heinnickel <john.heinnickel@thermofisher.com>
2025-08-25 08:15:37 +0000

    Attempting to reconcile lack of Channels provider visibility

--- function/src/cli/channels/Module.ts
+++ function/src/cli/channels/Module.ts
@@ -2,6 +2,19 @@ import { Module } from "@nestjs/common"
 import { CoroutinesModule } from "../../coroutines/di/Module.js"
 import { CliChannelsModuleTypes } from "./Types.js"
 
+console.log("Check Import Order :: ", [
+   CliChannelsModuleTypes.EnrollSourceFileCallChannel,
+   CliChannelsModuleTypes.RandomArtChannelCallChannel,
+   CliChannelsModuleTypes.EnrollSourceFileReplyChannel,
+   CliChannelsModuleTypes.RandomArtChannelReplyChannel,
+])
+console.log("Check Import Order :: ", {
+   [CliChannelsModuleTypes.EnrollSourceFileCallChannel]: 5,
+   [CliChannelsModuleTypes.RandomArtChannelCallChannel]: 6,
+   [CliChannelsModuleTypes.EnrollSourceFileReplyChannel]: 7,
+   [CliChannelsModuleTypes.RandomArtChannelReplyChannel]: 8,
+})
+
 @Module({
    imports: [
       CoroutinesModule.register({
@@ -14,18 +27,41 @@ import { CliChannelsModuleTypes } from "./Types.js"
                component: "BlockingChannel",
                concurrency: 3,
             },
-            [CliChannelsModuleTypes.EnrollSourceFileCallChannel]: {
+            "CliChannelsModuleTypes.EnrollSourceFileCallChannel": {
                component: "BlockingChannel",
                concurrency: 1,
             },
-            [CliChannelsModuleTypes.EnrollSourceFileReplyChannel]: {
+            "CliChannelsModuleTypes.EnrollSourceFileReplyChannel": {
                component: "BlockingChannel",
                concurrency: 1,
             },
          },
       }),
    ],
-   providers: [],
-   exports: [CoroutinesModule],
+   providers: [
+      {
+         provide: CliChannelsModuleTypes.EnrollSourceFileCallChannel,
+         useExisting: "CliChannelsModuleTypes.EnrollSourceFileCallChannel",
+      },
+      {
+         provide: CliChannelsModuleTypes.EnrollSourceFileReplyChannel,
+         useExisting: "CliChannelsModuleTypes.EnrollSourceFileReplyChannel",
+      },
+      {
+         provide: CliChannelsModuleTypes.RandomArtChannelCallChannel,
+         useExisting: "CliChannelsModuleTypes.RandomArtChannelCallChannel",
+      },
+      {
+         provide: CliChannelsModuleTypes.RandomArtChannelReplyChannel,
+         useExisting: "CliChannelsModuleTypes.RandomArtChannelReplyChannel",
+      },
+   ],
+   exports: [
+      // CoroutinesModule,
+      CliChannelsModuleTypes.EnrollSourceFileCallChannel,
+      CliChannelsModuleTypes.RandomArtChannelCallChannel,
+      CliChannelsModuleTypes.EnrollSourceFileReplyChannel,
+      CliChannelsModuleTypes.RandomArtChannelReplyChannel,
+   ],
 })
 export class CliChannelsModule {}
