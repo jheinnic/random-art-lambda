c530c2e9982741c489f1b05c9d254f7188056d95
John Heinnickel <john.heinnickel@thermofisher.com>
2025-09-09 18:16:18 +0000

    Atomic Type init

--- function/src/ipfs/di/IpfsModuleTypes.ts
+++ function/src/ipfs/di/IpfsModuleTypes.ts
@@ -1,11 +1,7 @@
-export const ABSTRACT_BLOCKSTORE: unique symbol = Symbol("Blockstore")
-export const LRU_CACHE: unique symbol = Symbol("LRU")
-export const FS_BLOCKSTORE_CONFIG: unique symbol = Symbol("FsBlockstoreConfiguration")
-
 export const IpfsModuleTypes = {
-  AbstractBlockstore: ABSTRACT_BLOCKSTORE,
-  FsBlockstoreConfiguration: FS_BLOCKSTORE_CONFIG,
-  LruCache: LRU_CACHE
+   AbstractBlockstore: Symbol("Blockstore"),
+   FsBlockstoreConfiguration: Symbol("FsBlockstoreConfig"),
+   LruCache: Symbol("LruCache"),
 }
 
 export const REGISTER_METHOD_KEY = "register"
