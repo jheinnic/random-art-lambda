c8fca0518b270feebf19f11b58bf975473d8c47a
John Heinnickel <john.heinnickel@thermofisher.com>
2025-10-25 17:35:58 +0000

    Muting some errors
    Silencing some standing issues in paint and CLI that are somewhat
    disruptive and experimental works in progress, or legacy code just not
    yet removed.

--- function/src/painting/di/Module.ts
+++ function/src/painting/di/Module.ts
@@ -6,8 +6,6 @@ import {
 } from "../../modules/index.js"
 import { PaintingModuleTypes } from "./Types.js"
 import { RandomArtTaskEngine } from "../components/RandomArtTaskEngine.js"
-import { DynamicModuleBlueprint } from './../../modules/di/DynamicModuleBlueprint';
-import { GenModelFactory } from "../components/GenModelFactory.js";
 
 const injectModuleTokens = {
    regionMapRepo: PaintingModuleTypes.InjectedRegionMapRepository,
@@ -22,12 +20,6 @@ const moduleHost = InjectableModuleClassFactory.create(
          builder.exportProviders({
             provide: PaintingModuleTypes.IRandomArtTaskEngine,
             useClass: RandomArtTaskEngine,
-         }, {
-            provide: PaintingModuleTypes.IGenModelSeedRegistry,
-            useClass: GenModelFactory
-         }, {
-            provide: PaintingModuleTypes.IGenModelFactory,
-            useExisting: PaintingModuleTypes.IGenModelSeedRegistry
          })
       }
    },
@@ -40,30 +32,15 @@ export class PaintingModule extends moduleHost.build() {
    private static ROOT_MODULE: DynamicModule
    static forRoot(config: PaintingModuleConfiguration): DynamicModule {
       if (PaintingModule.ROOT_MODULE !== undefined) {
-         throw new Error("Cannot create the root painting module multiple times...")
+         throw new Error(
+            "Cannot create the root painting module multiple times...",
+         )
+      } else {
+         console.log("Creating painting the first time")
       }
       PaintingModule.ROOT_MODULE = super.forRoot(config)
       return PaintingModule.ROOT_MODULE
    }
 
    // static forSeedTypes<T extends object>(config: , TxFn>)
-}
-
-
-export function packageSeedingModule<T>(txs: { [K in keyof T]: TxFn<T[K]> }) {
-   const builder = new DynamicModuleBlueprint(PaintingModule)
-   let tk
-   for (tk in Object.keys(txs)) {
-      builder.exportProviders(
-         Object.entries(txs).map((x) => {
-            return {
-               provide: x[0],
-               useFactory: (x: ),
-               inject: 
-            }
-         }))
-      )
-   }
-}
-   
 }
